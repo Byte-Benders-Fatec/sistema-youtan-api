afb8d79bddb0aff4df64d350767c9ca55ac3ea68
Thiago Ribeiro <65740833+yrnThiago@users.noreply.github.com>
2024-10-16 11:39:05 -0300

    feat: add form, question models

--- src/models/Forms.ts
+++ src/models/Forms.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import Question from "./Questions";
 
 @Entity('forms')
 
@@ -6,6 +7,15 @@ class Forms{
     @PrimaryGeneratedColumn('increment')
     id: number;
 
+    @Column('varchar', {
+        length: 100,
+        nullable: false
+    })
+    category: string;
+
+    @ManyToOne(() => Question, {eager: true})
+    @JoinColumn({"name": "id_question"})
+    Question: Question[];
 }
 
 export default Forms;
